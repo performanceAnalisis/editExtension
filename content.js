a8b86fc694d4dc3136610cf19e4d0de3e5644686
performanceAnalisis <86238498+performanceAnalisis@users.noreply.github.com>
2021-06-21 14:56:43 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,8 @@
+function edit() {
+
+document.querySelector("#example_wrapper > div.dt-buttons.btn-group.flex-wrap > button.btn.btn-secondary.buttons-selected.disabled").setAttribute("accesskey","q")
+
+
+}
+
+edit();
